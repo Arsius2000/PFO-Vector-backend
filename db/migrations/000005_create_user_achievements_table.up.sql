f8d2a2805980967dfdc040c6150cad2fac24e8a0
Arseniy <arsius2007@gmail.com>
2026-03-04 16:50:55 +0300

    Была создана таблицы user_achievements

---
+++ db/migrations/000005_create_user_achievements_table.up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE user_achievements IF NOT EXISTS (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    achievement_id INT NOT NULL,
+    awarded_at TIMESTAMPTZ DEFAULT NOW(),
+    awarded_by INT NOT NULL,
+
+    --связываем таблицу user_achievements и users
+    CONSTRAINT fk_achievements_user_id   
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    CONSTRAINT fk_achievements_awarded_by  
+        FOREIGN KEY (awarded_by) REFERENCES users(id)
+    --связываем таблицу user_achievements и achievements
+    CONSTRAINT fk_achievements_achievement_id
+        FOREIGN KEY (achievement_id) REFERENCES achievements(id)
+
+
+
+
+)
