a218bd5dfa766fc1f9c70076b38495de5cac139c
Arseniy <arsius2007@gmail.com>
2026-03-04 17:13:51 +0300

    Создана таблица events

---
+++ db/migrations/000006_create_events_table.up.sql
@@ -0,0 +1,15 @@
+--Создаем таблицу events
+CREATE TABLE IF NOT EXISTS events(
+    id SERIAL PRIMARY KEY,
+    event_date DATE NOT NULL,
+    start_time TIME,
+    end_time TIME,
+    title TEXT,
+    audience VARCHAR(150),
+    weight INT,
+    created_by INT NOT NULL,
+    --Связываем таблицу users и events
+    CONSTRAINT fk_events_user_id   
+        FOREIGN KEY (created_by) REFERENCES users(id)
+
+);
