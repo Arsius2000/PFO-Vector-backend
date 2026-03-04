9a75fe504777ddfc8c7cac8ce0103bb9fd9a123c
Arseniy <arsius2007@gmail.com>
2026-03-04 16:58:58 +0300

    Исправлен шаблон базы данных  user_achievements;

--- db/migrations/000005_create_user_achievements_table.up.sql
+++ db/migrations/000005_create_user_achievements_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user_achievements IF NOT EXISTS (
+CREATE TABLE  IF NOT EXISTS user_achievements(
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     achievement_id INT NOT NULL,
@@ -7,9 +7,9 @@ CREATE TABLE user_achievements IF NOT EXISTS (
 
     --связываем таблицу user_achievements и users
     CONSTRAINT fk_achievements_user_id   
-        FOREIGN KEY (user_id) REFERENCES users(id)
+        FOREIGN KEY (user_id) REFERENCES users(id),
     CONSTRAINT fk_achievements_awarded_by  
-        FOREIGN KEY (awarded_by) REFERENCES users(id)
+        FOREIGN KEY (awarded_by) REFERENCES users(id),
     --связываем таблицу user_achievements и achievements
     CONSTRAINT fk_achievements_achievement_id
         FOREIGN KEY (achievement_id) REFERENCES achievements(id)
@@ -17,4 +17,4 @@ CREATE TABLE user_achievements IF NOT EXISTS (
 
 
 
-)
+);
