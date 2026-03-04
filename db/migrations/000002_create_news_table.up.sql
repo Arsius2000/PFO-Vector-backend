b73513944fce4bc87fc7a145a7db6532af5b6280
Arseniy <arsius2007@gmail.com>
2026-03-04 12:00:55 +0300

    Была создана таблица news

---
+++ db/migrations/000002_create_news_table.up.sql
@@ -0,0 +1,17 @@
+-- Создаем таблицу News
+CREATE TABLE IF NOT EXISTS news(
+    id SERIAL PRIMARY KEY,
+    title varchar(255) NOT NULL,
+    short_desription varchar(255) NOT NULL,
+    full_desription TEXT NOT NULL,
+    news_date TIMESTAMP ,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    created_by INT NOT NULL,
+    --связываем таблицу news и users
+    CONSTRAINT fk_news_created_by   
+        FOREIGN KEY (created_by) REFERENCES users(id)
+);
+
+
+
+
