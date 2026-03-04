4dffacb921da32f1c9cc6f9edc42fb4c49493a4a
Arseniy <arsius2007@gmail.com>
2026-03-04 23:53:37 +0300

    Были написаны запросы для achievements

---
+++ db/queries/achievements.sql
@@ -0,0 +1,21 @@
+-- name: CreateAchievements :one
+INSERT INTO achievements(
+    achivements_name,
+    icon_name,
+    description,
+    condition_type,
+    condition_value
+)
+VALUES ($1, $2, $3, $4, $5)
+RETURNING *;
+
+
+-- name: UpdateAchievements :one
+UPDATE achievements
+SET achivements_name = $2,icon_name = $3,description = $4,condition_type = $5,condition_value = $6
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAchievements :exec
+DELETE FROM achievements
+WHERE id = $1;
